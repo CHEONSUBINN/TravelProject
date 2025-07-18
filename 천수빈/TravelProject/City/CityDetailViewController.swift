4bcb01ba56231cb0b7b6a8da7369691aebecc6a1
수빈 <svstnqls@gmail.com>
2025-07-18 14:09:11 +0900

    17일 과제 commit

---
+++ 천수빈/TravelProject/City/CityDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CityDetailViewController.swift
+//  TravelProject
+//
+//  Created by subin on 7/17/25.
+//
+
+import UIKit
+import Kingfisher
+
+class CityDetailViewController: UIViewController {
+    
+    var cityContent: City = City(city_name: "", city_english_name: "", city_explain: "", city_image: "", domestic_travel: false)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        
+    }
+    
+
+}
---
+++ 천수빈/TravelProject/Extension/File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  TravelProject
+//
+//  Created by subin on 7/17/25.
+//
+
+import UIKit
+
+
